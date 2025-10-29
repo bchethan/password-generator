3c61e28a7c3ec4ab814e7eb6d81af8d992b283aa
bchethan <157599837+bchethan@users.noreply.github.com>
2025-10-29 14:13:16 +0530

    Add password history feature and clear history functionality

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const chars = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 
                 "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "-", 
                 "+", "=", "{", "}", "[", "]", "\\", "\"", "'", ":", ";", ".",",", 
-                "/", "?", ">", "<", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "|"];
+                "/", "?", ">", "<", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "|"];
 
 const passwordEl = document.querySelector("#passwords-el")
 let passwordLength = document.querySelector("#password-length")
+const generateBtn = document.querySelector("#generate-btn")
+
+let newPasswords = []
+generateBtn.addEventListener("click", ()=>generatePassword())
+
 
 function generatePassword(){
     passwordEl.innerHTML = `
@@ -21,10 +26,10 @@ function generatePassword(){
     password1El.addEventListener("click", ()=>copyOnClick(password1El))
     password2El.addEventListener("click", ()=>copyOnClick(password2El))
 
-    let passwordItem1 = "";
-    let passwordItem2 = "";
+    let passwordItem1 = ``;
+    let passwordItem2 = ``;
 
-    const length = Math.max(passwordLength.value,15)
+    const length = Math.max(Number(passwordLength.value),15)
 
     for (let i = 0; i < length; i++){
         let randomIndex1 = Math.floor(Math.random()*chars.length);
@@ -34,10 +39,25 @@ function generatePassword(){
     }
     password1El.innerHTML = passwordItem1;
     password2El.innerHTML = passwordItem2;
+    storePasswords(passwordItem1)
+    storePasswords(passwordItem2)
     passwordLength.value = ""
 }
 
 function copyOnClick(password){
-    console.log("Copied to clipboard "+ password.textContent)
     navigator.clipboard.writeText(password.textContent);
 }
+
+function storePasswords(password){
+    let prevHistory = JSON.parse(localStorage.getItem("saved-passwords"));
+    console.log(newPasswords)
+    if(prevHistory){
+        newPasswords = prevHistory;
+        newPasswords.push(password);
+        localStorage.setItem("saved-passwords", JSON.stringify(newPasswords));
+    }else{
+        newPasswords = [password];
+        localStorage.setItem("saved-passwords", JSON.stringify(newPasswords));
+    }
+}
+
